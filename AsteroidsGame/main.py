312685d00a260bfd3c3bdecc9b644f897e234b7e
Abe Pasion <pasionabe@gmail.com>
2025-12-23 14:48:09 -0500

    Prints player

--- AsteroidsGame/main.py
+++ AsteroidsGame/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
@@ -10,16 +11,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_over = False
 
+    clock = pygame.time.Clock()
+    dt = 0
+    x, y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while game_over == False:
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
+        player.draw(screen)
+        
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
